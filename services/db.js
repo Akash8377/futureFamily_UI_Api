a9b1e62367f947cdf021e2a41fed7d1e5b7cbb77
Akash8377 <akashchoudhary8377@gmail.com>
2025-04-16 16:55:50 +0530

    port

--- services/db.js
+++ services/db.js
@@ -1,10 +1,40 @@
 const mysql = require("mysql");
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "future_family_ai",
+  host: "database-1.c54c86qsuhll.us-east-1.rds.amazonaws.com",
+  port: 3306, 
+  user: "admin",
+  password: "spreadsandlocks",
+  database: "bagels",
 });
 
-conn.connect();
+conn.connect((err) => {
+  if (err) {
+    console.error("Database connection error:", err.stack);
+    return;
+  }
+  console.log("Connected to database.");
+});
+
 module.exports = conn;
+
+
+conn.connect((err) => {
+  if (err) {
+    console.error("Database connection error:", err.stack);
+    return;
+  }
+  console.log("Connected to database.");
+});
+
+module.exports = conn;
+
+// const mysql = require("mysql");
+// const conn = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "future_family_ai",
+// });
+
+// conn.connect();
+// module.exports = conn;
